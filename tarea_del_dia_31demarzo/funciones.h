64460150d301dd19231c495941c27bc5418aba33
duartenolasco <91351461+duartenolasco@users.noreply.github.com>
2023-04-01 09:59:18 -0600

    Add files via upload

---
+++ tarea_del_dia_31demarzo/funciones.h
@@ -0,0 +1,15 @@
+//
+// Created by leste on 1/04/2023.
+//
+
+#ifndef TAREA_DEL_DIA_31DEMARZO_FUNCIONES_H
+#define TAREA_DEL_DIA_31DEMARZO_FUNCIONES_H
+
+#endif //TAREA_DEL_DIA_31DEMARZO_FUNCIONES_H
+using namespace std;
+void sumaelementos();
+void mayorelementos();
+void invertirorden();
+void mostrarsumaelementos();
+void numerodevecesfruta();
+void seleccion();
